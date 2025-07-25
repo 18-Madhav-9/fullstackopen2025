2d49ee318f5c5cf7272b00c8d6e40d3cf4f37e7a
18-madhav-9 <mash2172003@gmail.com>
2025-07-25 16:06:17 +0530

    Part1 unicafe 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,48 @@
+import { useState } from "react"
 
-const App = () => {
+const Display = (props) => {
   return (
-    <div>hello world</div>
+    <h1> {props.text} </h1>
+  )
+}
+
+const Button = (props) => {
+  return ( 
+    <button onClick={props.handler} > {props.text} </button>
+  )
+}
+
+const Stat = (props) => {
+  return (
+    <p>{props.text} {props.count} </p>
+  )
+}
+
+const App = (props) => {
+
+  const [good,setGood] = useState(0) 
+  const [neutral , setNeutral] = useState(0)
+  const [bad , setBad ] = useState(0)
+
+  const handleGood = ()=> {setGood(good+1)}
+  const handleNeutral = () => {setNeutral(neutral+1)}
+  const handleBad = () => {setBad(bad+1)}
+
+
+  return (
+    <div>
+      <Display text = {"give feedback"} />
+      
+      <Button handler = {handleGood} text = {"Good"} />
+      <Button handler = {handleNeutral} text = {"Neutral"} />
+      <Button handler = {handleBad} text= {"Bad"} />
+      
+      <Display text = {"statistics"} />
+      <Stat text= {"Good"} count= {good} />
+      <Stat text= {"Neutral"} count= {neutral} />
+      <Stat text= {"Bad"} count= {bad} />
+
+    </div>
   )
 }
 
