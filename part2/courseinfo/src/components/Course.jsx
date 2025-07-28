b1af7f69a86aac859c4b6194f46f5b8350869923
18-madhav-9 <mash2172003@gmail.com>
2025-07-28 22:31:59 +0530

    Part2 courseinfo after 2.2

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -11,12 +11,20 @@ const Part = ({part}) => {
         </li>
     )
 }
+const Total = ({parts}) => {
+    let total = 0 
+    parts.forEach(element => {total += element.exercises } );
+    return (
+        <p><b>total of {total} exercises</b></p>
+    )
+}
 
 const Course = ({course} ) => {
     return (
         <div>
             <Header header={course.name} />
             { course.parts.map ( (element) => < Part key = {element.id} part={element} />  ) }
+            <Total parts={course.parts} />
         </div>
     )
 } 
