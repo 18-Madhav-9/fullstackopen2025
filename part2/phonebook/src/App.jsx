ce8414cf1a4f6d5504442cf7dbe92cfa922eed37
18-madhav-9 <mash2172003@gmail.com>
2025-08-03 17:53:10 +0530

    Part2 phonebook after 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const ShowDetail = ({person}) =>{
   return (
@@ -43,17 +44,22 @@ const PersonForm = ({addperson,newName,nameChange,newNumber,numberChange}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNumber] = useState('')
   const [filter ,setfilter] = useState('')
-  
+
+
+  const getPersons = () => {
+    axios.get('http://localhost:3001/persons').then( (respone) => {
+      const data = respone.data 
+      setPersons(data)
+    })
+  }
+
+  useEffect(getPersons,[])
+
   const addperson = (event) => {
     event.preventDefault()
     const newperson = { 
