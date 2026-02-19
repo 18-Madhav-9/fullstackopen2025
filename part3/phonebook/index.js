295ec9405ff2cff396b1da5f5bdcb8cc9549d61d
mash <mash2172003@gmail.com>
2026-02-19 11:12:13 +0530

    Part3 and Part2  after exercise 3.9 connecting backend with frontend using cors

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,42 +1,48 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors()) 
 app.use(express.json())
 
 morgan.token('body', (req) => {
-  return req.method === 'POST' ? JSON.stringify(req.body) : '';
-});
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
+})
 
 app.use(
   morgan(':method :url :status :res[content-length] :response-time ms :body')
-);
+)
 
-persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+let persons = [
+  { 
+    id: "1",
+    name: "Arto Hellas", 
+    number: "040-123456"
+  },
+  { 
+    id: "2",
+    name: "Ada Lovelace", 
+    number: "39-44-5323523"
+  },
+  { 
+    id: "3",
+    name: "Dan Abramov", 
+    number: "12-43-234345"
+  },
+  { 
+    id: "4",
+    name: "Mary Poppendieck", 
+    number: "39-23-6423122"
+  }
 ]
 
-app.get('/api/persons',(request,response) => {
-    response.json(persons)
+app.get('/',(request,response) => {
+  response.json("OK")
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
 app.get('/info', (request, response) => {
@@ -47,53 +53,54 @@ app.get('/info', (request, response) => {
   `)
 })
 
-app.get('/api/persons/:id',(request,response) => {
-    const id = request.params.id
-    const person = persons.find(p => p.id == id )
-    if (person) {
-      response.json(person)
-    }
-    else{
-      response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(p => p.id == id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
-app.delete('/api/persons/:id',(request,response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  persons = persons.filter(p => p.id != id )
+  persons = persons.filter(p => p.id != id)
   response.status(204).end()
 })
 
 const generateId = () => {
-  return Math.floor(Math.random() * 1000000)
+  return Math.floor(Math.random() * 1000000).toString()
 }
 
-app.post('/api/persons',(request,response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body 
-  
-  if ( !body.name || !body.number ) {
+
+  if (!body.name || !body.number) {
     return response.status(400).json({ 
       error: 'name or number missing' 
     })
   }
-  
-  const names = persons.map(p => p.name )
-  if (names.includes(body.name)){
+
+  const names = persons.map(p => p.name)
+  if (names.includes(body.name)) {
     return response.status(400).json({ 
       error: 'name must be unique' 
     })
   }
 
   const person = {
-    id : generateId(),
-    name : body.name,
-    number : body.number
+    id: generateId(),
+    name: body.name,
+    number: body.number
   }
-  persons = persons.concat(person)
-  response.json(person);
 
+  persons = persons.concat(person)
+  response.json(person)
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001  
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
